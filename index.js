4e49e0f87b0ca2755acc7d64243e27c1dbae6a59
itsmtt <ithelpdesk.smtt@gmail.com>
2025-01-07 11:23:04 +0700

    Hitung buffer sebagai kombinasi ATR dan VWAP

--- index.js
+++ index.js
@@ -501,8 +501,8 @@ async function placeTakeProfitAndStopLoss(orders, atr, vwap, direction) {
       const { pricePrecision } = await getSymbolPrecision(symbol);
       const buffer =
         direction === "LONG"
-          ? atr + Math.abs(vwap - orderPrice) + orderPrice * 0.005
-          : atr + Math.abs(orderPrice - vwap) + orderPrice * 0.005;
+          ? atr + Math.abs(vwap -  orderPrice * 0.01)
+          : atr + Math.abs(orderPrice * 0.01 - vwap);
 
       // Hitung harga TP dan SL
       const takeProfitPrice =
