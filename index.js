6eb4f6bf73f706326b6330e682c7f9103e97939f
itsmtt <ithelpdesk.smtt@gmail.com>
2025-01-07 17:27:56 +0700

    update

--- index.js
+++ index.js
@@ -425,7 +425,6 @@ async function placeGridOrders(
     symbolInfo.filters.find((f) => f.tickSize).tickSize
   );
 
-  const adjustedGridSpacing = atr * (historicalVolatility > 0.03 ? 1.5 : 1.2);
   const volatility = atr / currentPrice;
   const adjustedGridCount = Math.max(
     2,
