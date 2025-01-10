65515906e7c709432cafd1fd0d5d00a6184822b2
itsmtt <ithelpdesk.smtt@gmail.com>
2025-01-10 15:59:54 +0700

    test update treshold

--- index.js
+++ index.js
@@ -366,17 +366,17 @@ async function determineMarketCondition(rsi, vwap, closingPrices, lastPrice) {
   // Log hasil sinyal fuzzy
   console.log(
     chalk.yellow(
-      `Fuzzy Signals: BUY = ${(buySignal * 100).toFixed(2)}% >= 80%, SELL = ${(
+      `Fuzzy Signals: BUY = ${(buySignal * 100).toFixed(2)}% >= 60%, SELL = ${(
         sellSignal * 100
-      ).toFixed(2)}% >= 80%`
+      ).toFixed(2)}% >= 60%`
     )
   );
 
   // Tentukan kondisi pasar berdasarkan sinyal
-  if (buySignal > sellSignal && buySignal >= 0.8) {
+  if (buySignal > sellSignal && buySignal >= 0.6) {
     console.log(`Posisi sekarang LONG (indikator menunjukkan peluang beli).`);
     return "LONG";
-  } else if (sellSignal > buySignal && sellSignal >= 0.8) {
+  } else if (sellSignal > buySignal && sellSignal >= 0.6) {
     console.log(`Posisi sekarang SHORT (indikator menunjukkan peluang jual).`);
     return "SHORT";
   } else {
