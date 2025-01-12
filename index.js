3ad1f234dc727c942cce0e526191468dcbcf1396
itsmtt <ithelpdesk.smtt@gmail.com>
2025-01-12 19:26:24 +0700

    Update candle interval from 1h to 4h in trade function

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ async function trade() {
     // Mengambil data candle
     const candles = await client.futuresCandles({
       symbol: SYMBOL,
-      interval: "1h",
+      interval: "4h",
     });
 
     // validasi candles
