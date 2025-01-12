9a24474a59f1292b6f1a88067a73159d89246d98
Hafriyanto <142964006+itsmtt@users.noreply.github.com>
2025-01-12 10:19:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -480,7 +480,7 @@ async function placeTakeProfitAndStopLoss(orders, atr, direction) {
       const { pricePrecision } = await getSymbolPrecision(symbol);
       
       // Hitung buffer atr untuk TP dan SL
-      const buffer = atr;
+      const buffer = orderPrice * atr;
 
       // Hitung harga TP dan SL
       const takeProfitPrice =
