060e21b069233d68bbcccaa6ebe2729d8a4715b4
itsmtt <ithelpdesk.smtt@gmail.com>
2025-01-23 08:50:59 +0700

    Set fixed callbackRate to 2.5 in placeTrailingStop function

--- index.js
+++ index.js
@@ -635,7 +635,7 @@ async function placeTrailingStop(order, atr, direction) {
       side: direction === "LONG" ? "SELL" : "BUY",
       type: "TRAILING_STOP_MARKET",
       activationPrice: roundedTrailingStop,
-      callbackRate: buffer,
+      callbackRate: 2.5,
       quantity,
       reduceOnly: true,
     });
